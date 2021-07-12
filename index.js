b0ca1038b78b81b89910663bb59ca5abd2523884
AlexanderKllanxhja <akllanxhja@gmail.com>
2021-07-12 08:32:59 -0600

    commit tree b9dfd125bf8e52481f4b18a90210cd520396ed41

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 32;
+let num4 = 7;
+const mod = num3 % num4;
+
+const max = Math.max(1, 5, 7, 10, 19, 20);
+
 
 // Write your code here
